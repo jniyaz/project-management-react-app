208e88da5575668290e7df5f8fa3d0ecf02589f1
Niyaz <jniyaz@gmail.com>
2020-06-08 03:35:30 +0800

    design layouts fixed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/projects/ProjectDetails";
+import SingIn from "./components/auth/SignIn";
+import SingUp from "./components/auth/SignUp";
+import CreateProject from "./components/projects/CreateProject";
 
 function App() {
   return (
@@ -11,7 +14,10 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Dashboard} />
-          <Route path="/project/:id" component={ProjectDetails} />
+          <Route exact path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin" component={SingIn} />
+          <Route path="/signup" component={SingUp} />
+          <Route path="/create" component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+  state = {
+    name: "",
+    email: "",
+    password: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <form className="white" onSubmit={this.handleSubmit}>
+          <h5 className="grey-text text-darken-3">Sign Up</h5>
+          <div className="input-field">
+            <label htmlFor="name">Full Name</label>
+            <input
+              type="text"
+              id="name"
+              className=""
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              className=""
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              className=""
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="cpassword">Confirm Password</label>
+            <input
+              type="password"
+              id="confirm_password"
+              className=""
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
