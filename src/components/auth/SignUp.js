a127b25cd6897ce73043a68b0b01c8c03618a09a
Niyaz <jniyaz@gmail.com>
2020-06-09 21:16:43 +0800

    user signup implementation

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { signUp } from "../../store/actions/authActions";
 
 class SignUp extends Component {
   state = {
-    name: "",
+    firstName: "",
+    lastName: "",
     email: "",
     password: "",
   };
@@ -17,21 +19,33 @@ class SignUp extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    // console.log(this.state);
+    this.props.signUp(this.state);
   };
 
   render() {
-    const { auth } = this.props;
+    const { auth, authError } = this.props;
     if (auth.uid) return <Redirect to="/" />;
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
           <h5 className="grey-text text-darken-3">Sign Up</h5>
           <div className="input-field">
-            <label htmlFor="name">Full Name</label>
+            <label htmlFor="fname">First Name</label>
             <input
               type="text"
-              id="name"
+              required
+              id="firstName"
+              className=""
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="lname">Last Name</label>
+            <input
+              type="text"
+              id="lastName"
+              required
               className=""
               onChange={this.handleChange}
             />
@@ -41,6 +55,7 @@ class SignUp extends Component {
             <input
               type="email"
               id="email"
+              required
               className=""
               onChange={this.handleChange}
             />
@@ -51,10 +66,11 @@ class SignUp extends Component {
               type="password"
               id="password"
               className=""
+              required
               onChange={this.handleChange}
             />
           </div>
-          <div className="input-field">
+          {/* <div className="input-field">
             <label htmlFor="cpassword">Confirm Password</label>
             <input
               type="password"
@@ -62,9 +78,12 @@ class SignUp extends Component {
               className=""
               onChange={this.handleChange}
             />
-          </div>
+          </div> */}
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+            <div className="red-text center">
+              {authError ? <p>{authError}</p> : null}
+            </div>
           </div>
         </form>
       </div>
@@ -75,7 +94,14 @@ class SignUp extends Component {
 const mapStateToProps = (state) => {
   return {
     auth: state.firebase.auth,
+    authError: state.auth.authError,
   };
 };
 
-export default connect(mapStateToProps)(SignUp);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUp: (newUser) => dispatch(signUp(newUser)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
