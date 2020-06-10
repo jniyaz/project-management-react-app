6c64fd2b14596ee5a412fb99d28f8f608b71677c
Niyaz <jniyaz@gmail.com>
2020-06-10 12:09:50 +0800

    projects, notifications order by desc fixed

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -60,7 +60,7 @@ const mapStateToProps = (state) => {
 export default compose(
   connect(mapStateToProps),
   firestoreConnect([
-    { collection: "projects" },
-    { collection: "notifications", limit: 3 },
+    { collection: "projects", limit: 10, orderBy: ["createdAt", "desc"] },
+    { collection: "notifications", limit: 5, orderBy: ["time", "desc"] },
   ])
 )(Dashboard);
