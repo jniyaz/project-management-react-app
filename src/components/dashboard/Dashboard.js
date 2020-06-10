dc82cba7ca8a05c03937bbfc1fdf89a2ae125333
Niyaz <jniyaz@gmail.com>
2020-06-10 12:04:13 +0800

    realtime notifications with firebase fixed

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -8,7 +8,7 @@ import { Redirect } from "react-router-dom";
 
 class Dashboard extends Component {
   render() {
-    const { projects, auth } = this.props; // destructure
+    const { projects, auth, notifications } = this.props; // destructure
     if (!auth.uid) return <Redirect to="/signin" />;
     if (projects) {
       return (
@@ -18,7 +18,7 @@ class Dashboard extends Component {
               <ProjectList projects={projects} />
             </div>
             <div className="col s12 m5 offset-m1">
-              <Notifications />
+              <Notifications notifications={notifications} />
             </div>
           </div>
         </div>
@@ -53,10 +53,14 @@ const mapStateToProps = (state) => {
   return {
     projects: state.firestore.ordered.projects,
     auth: state.firebase.auth,
+    notifications: state.firestore.ordered.notifications,
   };
 };
 
 export default compose(
   connect(mapStateToProps),
-  firestoreConnect([{ collection: "projects" }])
+  firestoreConnect([
+    { collection: "projects" },
+    { collection: "notifications", limit: 3 },
+  ])
 )(Dashboard);
